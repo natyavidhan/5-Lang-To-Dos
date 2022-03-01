d89748952e80887c9851e73d733521c6eedcb223
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-03-01 21:37:26 +0530

    java todo complete

--- Java ToDo/src/ToDoForm.java
+++ Java ToDo/src/ToDoForm.java
@@ -2,23 +2,43 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ToDoForm {
+public class ToDoForm extends JFrame {
     private JTextField textField1;
     private JButton addTaskButton;
     private JList list1;
     private JButton deleteTaskButton;
+    private JPanel panelMain;
+    DefaultListModel listModel = new DefaultListModel();
 
     public ToDoForm() {
         addTaskButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //add the text in the text field to the list
                 String task = textField1.getText();
-                DefaultListModel model = (DefaultListModel) list1.getModel();
-                model.addElement(task);
+                listModel.addElement(task);
+                list1.setModel(listModel);
+
                 textField1.setText("");
                 
             }
         });
+        deleteTaskButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //remove selected item from list
+                int selectedIndex = list1.getSelectedIndex();
+                if (selectedIndex != -1) {
+                    listModel.remove(selectedIndex);
+                }
+                list1.setModel(listModel);
+            }
+        });
+    }
+    public static void main(String[] args) {
+        ToDoForm form = new ToDoForm();
+        form.setContentPane(form.panelMain);
+        form.setSize(400, 400);
+        form.setVisible(true);
+        form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
